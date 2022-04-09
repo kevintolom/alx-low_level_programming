2f198aa54d3de4ba6ee0df50ffc7b223a8e928ba
kevintolom <kxtolom@gmail.com>
2022-04-08 20:39:28 -0700

    task 11 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main Print all possible combinations of three different digits,
- * in ascending order, separated by a comma followed by a space
+ * main Prints all possible combinations of three different digits,
+ * 	in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0
  */
