995fe0652889a8ad5e3e3aed0f623d36c542ab13
kevintolom <kxtolom@gmail.com>
2022-04-18 04:10:01 -0700

    main file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_numbers(int n);
-
-#endif
