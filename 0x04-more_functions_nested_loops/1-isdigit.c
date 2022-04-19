d9a5f66d7ea9a48ce958e388572b24e10e5f382c
kevintolom <kxtolom@gmail.com>
2022-04-19 15:22:49 -0700

    task 1 commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,7 +5,15 @@
  * @num: int to be checked
  * Return: 1 if num is a digit, 0 otherwise
  */
-int _isdigit(int num)
+int _isdigit(int c)
 {
-	return (num >= '0' && num <= '9');
+if (c >= 48 && c <= 57)
+{
+return (1);	
+}
+else
+{
+return (0):
+}
+_putchar('\n');
 }
