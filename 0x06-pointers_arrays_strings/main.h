edf01b00db67fa6fd23cf63abdc2bc25dbe8a972
kevintolom <kxtolom@gmail.com>
2022-04-22 15:59:14 -0700

    test commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -14,5 +12,3 @@ char *rot13(char *a);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
-#endif
